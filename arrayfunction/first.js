d5a792e5d0ff71d0d205ae04267c81bb3d313ec1
himanshu983 <dev.anshu086@gmail.com>
2024-07-16 11:21:13 +0530

    my functions

---
+++ arrayfunction/first.js
@@ -0,0 +1,13 @@
+const profile = {
+    username : 'himanshu',
+    isFollow: 'yes',
+    followers:980,
+    following:110,
+};
+console.log(profile);
+//conditional statement
+
+let age = 20;
+
+let result = age >= 18? "adult" : " not adult";
+console.log(result);
