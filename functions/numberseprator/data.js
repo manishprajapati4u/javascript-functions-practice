e38a3b487fd87c534eb6446b8c00f35f2ba94ac0
himanshu983 <dev.anshu086@gmail.com>
2024-07-18 00:04:11 +0530

    done

--- functions/numberseprator/data.js
+++ functions/numberseprator/data.js
@@ -3,8 +3,29 @@ document.getElementById("button").addEventListener("click", function(){
      const inputValue = document.getElementById("inputValue").value;
      const finalValue = remover(inputValue, true) 
     document.getElementById("value").innerHTML = finalValue; 
+    
+    //storing of inputvalues in localstorage
+    let storedValues = JSON.parse(localStorage.getItem("storedValues")) || [];
+    storedValues.push(finalValue);
+    localStorage.setItem("storedValues", JSON.stringify(storedValues));
 
+    displayStoredValues();
 });
+function displayStoredValues() {
+    let storedData = localStorage.getItem('storedValues');
+    if (storedData) {
+        let parseData = JSON.parse(storedData);
+        let output = document.getElementById("storedValuesList");
+        output.innerHTML = "";
+
+        parseData.forEach(function(value) {
+            let listItem = document.createElement("li");
+            listItem.textContent = value;
+            output.appendChild(listItem);
+        });
+    }
+}
+
 
 function remover(seprater,decimal){
     try{
@@ -16,14 +37,8 @@ function remover(seprater,decimal){
     }
     catch(error){
         return "";
-    }
+    }   
 } 
-function appendFormattedValue(value) {
-    const valueContainer = document.getElementById("value");
-    const currentValue = valueContainer.innerHTML;
-    const newValue = currentValue + "<br>" + value;
-    valueContainer.innerHTML = newValue;
-}
 
 }
 
